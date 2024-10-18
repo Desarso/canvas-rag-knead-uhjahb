b8c8478eb4e9cce5de718a585446e9244b8ae868
desarso <malek.gabriel33@gmail.com>
2024-10-18 15:42:38 -0700

    backend changes

---
+++ backend/canvas.py
@@ -0,0 +1,43 @@
+# Import the Canvas class
+from canvasapi import Canvas
+from datetime import datetime
+
+# Canvas API URL
+API_URL = "https://csus.instructure.com"
+# Canvas API key
+API_KEY = "11299~ZyMLXJUVk4F8mX6FE7cXZGnYZtDL77794hFV3Y9JyzWmLTBQNX4ryPQTVFe3ECAD"
+
+# Initialize a new Canvas object
+canvas = Canvas(API_URL, API_KEY)
+
+
+
+user = canvas.get_user('self')
+courses = user.get_courses(enrollment_status='active',include =['favorites'])
+
+
+def get_favorite_courses():
+    favorite_courses=[]
+
+    for course in courses:
+        if hasattr(course, 'name'):
+            if course.is_favorite:
+                favorite_courses.append(course)
+                
+    return favorite_courses
+
+def custom_serializer(obj):
+    if isinstance(obj, datetime):
+        return obj.isoformat() 
+    if isinstance(obj, object):  
+        return str(obj.__dict__)  
+    raise TypeError(f"Type {type(obj)} not serializable")
+
+
+def get_files_from_course(id: str):
+    course = canvas.get_course(id)
+    files = course.get_files()
+    return files
+
+courses = get_favorite_courses()
+print(courses[0].id)
